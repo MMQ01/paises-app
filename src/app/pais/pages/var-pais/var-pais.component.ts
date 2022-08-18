20132c92281a9122226eb2179dd2900ee0688532
Marcelo <marcelomorales2001@gmail.com>
2022-08-17 22:33:52 -0500

    primer commit

---
+++ src/app/pais/pages/var-pais/var-pais.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-var-pais',
+  templateUrl: './var-pais.component.html',
+  styles: [
+  ]
+})
+export class VarPaisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
